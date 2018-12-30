02cbaaab194f916755ab4d7397f9e89ebac7a8d2
s-yoshino <shou-y6216@yoshinoda.com>
2018-12-31 00:13:45 +0900

    [add] エントリポイントを追加しました。

---
+++ WindowsFormUsage/MainRunner.vb
@@ -0,0 +1,15 @@
+﻿Module MainRunner
+    <STAThread()>
+    Sub Main(args As String())
+        If args.Length = 0 Then
+            MsgBox("画面IDを指定してください。")
+            Exit Sub
+        End If
+
+        Dim screenId As String = args(0)
+
+        MsgBox(screenId)
+
+        Application.Run(New Form1)
+    End Sub
+End Module
