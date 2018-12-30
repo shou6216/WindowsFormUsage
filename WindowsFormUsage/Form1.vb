fe1da02e22d655ee7ae6b310603d621f000e60f0
s-yoshino <shou-y6216@yoshinoda.com>
2018-12-30 22:59:22 +0900

    [add] フォームに処理ボタンと閉じるボタンを追加

--- WindowsFormUsage/Form1.vb
+++ WindowsFormUsage/Form1.vb
@@ -1,3 +1,12 @@
 ﻿Public Class Form1
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+    End Sub
 
+    Private Sub Button1_Click_1(sender As Object, e As EventArgs) Handles Button1.Click
+        MsgBox("処理1を実行しました。")
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        Me.Close()
+    End Sub
 End Class
